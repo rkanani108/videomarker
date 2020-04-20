9f9c9c0b436162dbd27e1d976903a61f38eaef51
Rakesh Kanani <rkanani@horizontal.com>
2020-04-20 14:02:39 +0530

    added speech

--- js/interaction.js
+++ js/interaction.js
@@ -1,3 +1,72 @@
+//Gujrati typing
+
+
+
+
+
+
+
+//speech recognisation
+
+ var speech = {
+      start : function () {
+      // speech.start() : start speech recognition
+
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        speech.recognition = new SpeechRecognition();
+        speech.recognition.continuous = true;
+        speech.recognition.interimResults = false;
+        speech.recognition.lang = "gu-GU";
+        speech.recognition.onerror = function (evt) {
+          console.log(evt);
+        };
+        speech.recognition.onresult = function (evt) {
+          document.getElementById('description').value=   document.getElementById('description').value+" "+evt.results[0][0].transcript;
+          speech.stop();
+        };
+        speech.recognition.start();
+        document.getElementById('search-on').disabled = true;
+        document.getElementById('search-off').disabled = false;
+      },
+
+      stop : function () {
+      // speech.stop() : end speech recognition
+
+        if (speech.recognition != null) {
+          speech.recognition.stop();
+          speech.recognition = null;
+          document.getElementById('search-on').disabled = false;
+          document.getElementById('search-off').disabled = true;
+        }
+      }
+    };
+
+    window.addEventListener("load", function () {
+      // [1] CHECK IF BROWSER SUPPORTS SPEECH RECOGNITION
+      if (window.hasOwnProperty('SpeechRecognition') || window.hasOwnProperty('webkitSpeechRecognition')) {
+        document.getElementById("search-speech").style.display = "block";
+        document.getElementById("search-on").disabled = false;
+
+        // [2] ASK FOR USER PERMISSION TO ACCESS MICROPHONE
+        // WILL ALSO FAIL IF NO MICROPHONE IS ATTACHED TO COMPUTER
+        // navigator.mediaDevices.getUserMedia({ audio: true })
+        // .then(function(stream) {
+          // document.getElementById("search-on").disabled = false;
+        // })
+        // .catch(function(err) {
+          // document.getElementById("search-speech").innerHTML = "Please enable access and attach a microphone";
+        // });
+      }
+    });
+
+
+
+
+
+
+
+
+
 $( document ).ready(function() {
          refreshGrid();
 		 var fileName=localStorage.getItem('filename');
