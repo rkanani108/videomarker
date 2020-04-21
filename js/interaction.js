72a9e7640255776f38fbddfb43bc290f08c4464e
Rakesh Kanani <rkanani@horizontal.com>
2020-04-21 12:24:11 +0530

    update UI and added links

--- js/interaction.js
+++ js/interaction.js
@@ -8,57 +8,57 @@
 
 //speech recognisation
 
- var speech = {
-      start : function () {
-      // speech.start() : start speech recognition
-        var txt=document.querySelector('#description').value;
-        const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-        speech.recognition = new SpeechRecognition();
-        speech.recognition.continuous = true;
-        speech.recognition.interimResults = false;
-        speech.recognition.lang = "gu";
-        speech.recognition.onerror = function (evt) {
-          console.log(evt);
-        };
-        speech.recognition.onresult = function (evt) {
+ // var speech = {
+      // start : function () {
+      // // speech.start() : start speech recognition
+        // var txt=document.querySelector('#description').value;
+        // const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+        // speech.recognition = new SpeechRecognition();
+        // speech.recognition.continuous = true;
+        // speech.recognition.interimResults = false;
+        // speech.recognition.lang = "gu";
+        // speech.recognition.onerror = function (evt) {
+          // console.log(evt);
+        // };
+        // speech.recognition.onresult = function (evt) {
 			
-          document.querySelector('#description').value = txt + " "+evt.results[0][0].transcript;
-          speech.stop();
-        };
-        speech.recognition.start();
-        document.getElementById('search-on').disabled = true;
-        document.getElementById('search-off').disabled = false;
-      },
+          // document.querySelector('#description').value = txt + " "+evt.results[0][0].transcript;
+          // speech.stop();
+        // };
+        // speech.recognition.start();
+        // document.getElementById('search-on').disabled = true;
+        // document.getElementById('search-off').disabled = false;
+      // },
 
-      stop : function () {
-      // speech.stop() : end speech recognition
+      // stop : function () {
+      // // speech.stop() : end speech recognition
 
-        if (speech.recognition != null) {
-          speech.recognition.stop();
-          speech.recognition = null;
-          document.getElementById('search-on').disabled = false;
-          document.getElementById('search-off').disabled = true;
-        }
-      }
-    };
+        // if (speech.recognition != null) {
+          // speech.recognition.stop();
+          // speech.recognition = null;
+          // document.getElementById('search-on').disabled = false;
+          // document.getElementById('search-off').disabled = true;
+        // }
+      // }
+    // };
 
-    window.addEventListener("load", function () {
-      // [1] CHECK IF BROWSER SUPPORTS SPEECH RECOGNITION
-      if (window.hasOwnProperty('SpeechRecognition') || window.hasOwnProperty('webkitSpeechRecognition')) {
-        document.getElementById("search-speech").style.display = "block";
-        document.getElementById("search-on").disabled = false;
+    // window.addEventListener("load", function () {
+      // // [1] CHECK IF BROWSER SUPPORTS SPEECH RECOGNITION
+      // if (window.hasOwnProperty('SpeechRecognition') || window.hasOwnProperty('webkitSpeechRecognition')) {
+        // document.getElementById("search-speech").style.display = "block";
+        // document.getElementById("search-on").disabled = false;
 
-        // [2] ASK FOR USER PERMISSION TO ACCESS MICROPHONE
-        // WILL ALSO FAIL IF NO MICROPHONE IS ATTACHED TO COMPUTER
-         navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(function(stream) {
-         document.getElementById("search-on").disabled = false;
-         })
-         .catch(function(err) {
-          document.getElementById("search-speech").innerHTML = "Please enable access and attach a microphone";
-         });
-      }
-    });
+        // // [2] ASK FOR USER PERMISSION TO ACCESS MICROPHONE
+        // // WILL ALSO FAIL IF NO MICROPHONE IS ATTACHED TO COMPUTER
+         // navigator.mediaDevices.getUserMedia({ audio: true })
+        // .then(function(stream) {
+         // document.getElementById("search-on").disabled = false;
+         // })
+         // .catch(function(err) {
+          // document.getElementById("search-speech").innerHTML = "Please enable access and attach a microphone";
+         // });
+      // }
+    // });
 
 
 
